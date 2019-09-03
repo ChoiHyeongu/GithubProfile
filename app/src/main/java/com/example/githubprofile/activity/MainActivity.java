5f82d4fb079b12802688194521deff1fec08f992
ChoiHyeongu <a11dsd123@gmail.com>
2019-09-04 01:02:43 +0900

    [Package] Classify by packages

--- app/src/main/java/com/example/githubprofile/activity/MainActivity.java
+++ app/src/main/java/com/example/githubprofile/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.githubprofile;
+package com.example.githubprofile.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.githubprofile.R;
 
 public class MainActivity extends AppCompatActivity {
 
