5ea23358f0169612f9f6f68f0c92ab310cf611de
ChoiHyeongu <a11dsd123@gmail.com>
2019-09-04 01:08:55 +0900

    [Acitivity] Connect presenter

--- app/src/main/java/com/example/githubprofile/activity/MainActivity.java
+++ app/src/main/java/com/example/githubprofile/activity/MainActivity.java
@@ -4,12 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.githubprofile.R;
+import com.example.githubprofile.presenter.Presenter;
+import com.example.githubprofile.presenter.ProfilePresenter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Presenter.View {
+
+    ProfilePresenter profilePresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profilePresenter = new ProfilePresenter();
+        profilePresenter.attachView(this);
+
+        profilePresenter.loadUser();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        profilePresenter.detachView();
     }
 }
